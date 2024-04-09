aeac1d9de2762090b918b14fcb2bdf920876f39c
Prabin Bohara <prabin.bohara@fusemachines.com>
2024-04-09 14:33:27 +0545

    Implementation of the calculation functions

--- Git/app.py
+++ Git/app.py
@@ -1,3 +1,5 @@
+from calculation import addition, subtraction, multiplicaion, division
+
 def operation_type():
     '''
     Takes input for the type of calcuation
@@ -6,10 +8,10 @@ def operation_type():
     num(int): the opertion number
     '''
     print("Type the number associated withe the operation:\n")
-    print("1) Addition\n")
-    print("2) Subtraction\n")
-    print("3) Multiplication\n")
-    print("4) Division\n")
+    print("1) Addition")
+    print("2) Subtraction")
+    print("3) Multiplication")
+    print("4) Division")
     num = input()
     return num
 
@@ -24,14 +26,33 @@ def take_inputs():
     num1(int or float): The first number for calculation purpose
     num2(int or float): Second number for required calculation
     '''
-    num1 = input()
-    num2 = input()
+    print("\nEnter the first number : ")
+    num1 = int(input())
+    print("Enter the second number : ")
+    num2 = int(input())
     return num1, num2
 
 
-########### Main:
-operation = operation_type()
-num1, num2 = take_inputs()
-#print(operation, num1, num2)
+def main():
+    ########### Main:
+    operation = int(operation_type())
+    num1, num2 = take_inputs()
+    #print(operation, num1, num2)
 
+    if(operation == 1):
+        result = addition(num1, num2)
+    elif(operation == 2):
+        result = subtraction(num1, num2)
+    elif(operation == 3):
+        result = multiplicaion(num1, num2)
+    elif(operation == 4):
+        result = division(num1, num2)
+    else:
+        print(operation)
+        print("Invalid operation type")
+        return
+    
+    print("\nThe output of the opeartion is : ", result)
+    
 
+main()
