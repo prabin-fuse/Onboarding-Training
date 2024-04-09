b88f7d677686158a7223b6dcb7f4dd588d5b5845
Prabin Bohara <prabin.bohara@fusemachines.com>
2024-04-09 12:59:07 +0545

    Addition, subtraction and multiplication created
    Each function takes two respective inputs and generate the output as
    described in the function. Not made for array of numbers.

---
+++ Git/calculation.py
@@ -0,0 +1,41 @@
+def addition(num1, num2):
+    '''
+    Addition of two numbers.
+
+    Parameters:
+    num1(int or float): The first number to be added.
+    num2(int or float): The second number to be added.
+
+    Returns:
+    int or float: The sum of two input numbers
+    '''
+    return num1 + num2
+
+
+def subtraction(num1, num2):
+    '''
+    Subtraction of two given numbers.
+
+    Parameters:
+    num1(int or float): Minuend of the subtraction operation
+    num2(int or float): Subtrahend of the subtraction operation
+
+    Returns:
+    int or float: The difference of two numbers.
+    Case1: The return number is positive if Minuend >= Subtrahend
+    Case2: The retrn number is negative if Minuend < Subtrahend
+    '''
+    return num1 - num2
+
+def multiplicaion(num1, num2):
+    '''
+    Multiplication of two given numbers.
+
+    Parameters:
+    num1(int or float): Multiplicand for the multiplication operation
+    num2(int or float) : Multiplier for the multiplication
+
+    Returns:
+    int or float: The product of two numbers
+    '''
+    return num1 * num2
---
+++ Git/readme.sh
@@ -0,0 +1,15 @@
+1. GIT
+
+#Write Python functions for basic mathematical operations such as addition, subtraction, and multiplication. These functions should take two numeric inputs and return the result of the corresponding operation.
+Initialize the git. 
+Track the files
+Check Status
+Check the staging area
+Create a branch named develop
+Commit the code with a good commit message
+Push the code. Check the push results on git. 
+Create a branch called bugfix
+Add a feature to divide two numbers
+Commit the code and Push the code in the new branch bugfix
+Create a PR request to merge code on develop branch 
+Merge the PR
